8ba5e9b66ad7b8472d5b153b08a61d4ca2731a94
BluePawDev <jaruby@me.com>
2017-05-18 15:56:14 -0500

    Add base normalize and base css, initialized JS/JQ

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('JS/JQ sourced!');
+
+
+// click handler for addBlock()
+$(document).ready(function(){
+  $('.addBlock').on('click', function()){
+    console.log('in addBlock on click');
+    // append a block to DOM
+    var appendString = "<div class='blocky'></div>"
+    $('#outputDiv').append(appendString);
+    // with a random color
+  }
+
+}
